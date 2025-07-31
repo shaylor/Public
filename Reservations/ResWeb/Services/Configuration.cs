515784b8d403adb6350431d8212db0066a204763
Shaylor Merritt <shaylormerritt@yahoo.com>
2025-07-31 09:54:08 -0400

    Updated ConfigurationService to load and save through a Configuration Model dictionary to preserve EF Core relations. Added UI for managing Configuration Properties.

--- Reservations/ResWeb/Services/Configuration.cs
+++ Reservations/ResWeb/Services/Configuration.cs
@@ -18,7 +18,30 @@ namespace ResWeb.Services
             _scopeFactory = scopeFactory;
         }
 
-        public TimeSpan ReminderTime { get; set; }
+        private Dictionary<string, Models.Configuration> _configItems = new Dictionary<string, Models.Configuration>();
+
+        public TimeSpan ReminderTime
+        {
+            get
+            {
+                var configItem = _configItems.FirstOrDefault(c => c.Key == "ReminderTime").Value;
+                if (TimeSpan.TryParse(configItem.Value ?? "",out var timeSpan))
+                {
+                    return timeSpan;
+                }
+
+                return new TimeSpan(0, 15, 0); // Default return, if configItem is misconfigured.
+            }
+            set
+            {
+                var configItem = _configItems.FirstOrDefault(c => c.Key == "ReminderTime");
+                if (configItem.Value != null)
+                {
+                    configItem.Value.Value = value.ToString();
+                }
+            }
+        }
+
 
         /// <summary>
         /// Reads from Configuration datatable and attempts to fill properties of the service.
@@ -27,11 +50,19 @@ namespace ResWeb.Services
         /// <returns></returns>
         public async Task LoadAsync(CancellationToken cancellationToken = default)
         {
+            // Clear the dictionary before using it.
+            _configItems = new Dictionary<string, Models.Configuration>();
+
             // dbContext scope cannot be used within a singleton service. Instead declare new scope for use during loads only.
             var scope = _scopeFactory.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
             var results = await dbContext.Configuration.FromSqlRaw<Models.Configuration>("EXEC [uspGetAllConfiguration]").ToListAsync(cancellationToken);
+            foreach (var configItem in results)
+            {
+                _configItems.Add(configItem.Name, configItem);
+            }
+
             var properties = typeof(Configuration).GetProperties();
 
             // Loop through all properties. Attempt to map configuration values to properties by name.
@@ -65,6 +96,20 @@ namespace ResWeb.Services
                     _logger.LogWarning($"Error setting property {property.Name}: No configuration found for this property.");
                 }
             }
+        }
+
+        public async Task SaveAsync(CancellationToken cancellationToken = default)
+        {
+            // Declare dbContext scope.
+            var scope = _scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            // All properties write back to the _configItems dictionary. Use this to update the database.
+            // NOTE: Using properties directly will not work as they are not tracked by EF Core.
+            var configItems = _configItems.Values.ToList();
+            dbContext.Configuration.UpdateRange(configItems);
+
+            await dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
         }
     }
 }
