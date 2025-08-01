e564ab01b620f5915f34a9d53257494d844bdeb8
Shaylor Merritt <shaylormerritt@yahoo.com>
2025-08-01 12:52:55 -0400

    Added basic user role management for Admin users.

---
+++ Reservations/ResWeb/Models/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using ResWeb.Data;
+
+namespace ResWeb.Models
+{
+    public class UserRole
+    {
+        public ApplicationUser User { get; set; }
+        public string RoleId { get; set; }
+    }
+}
